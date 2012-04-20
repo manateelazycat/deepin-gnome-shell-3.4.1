fe8ec5fe4e0e194b66bacf0fd6fac8b14cb96fe7
Andy Stewart <lazycat.manatee@gmail.com>
2012-04-20 16:01:22 +0800

    Move app filter to left of screen.

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -156,8 +156,8 @@ const ViewByCategories = new Lang.Class({
                                                    y_fill: false,
                                                    style_class: 'vfade' });
         this._categoryScroll.add_actor(this._categoryBox);
-        this.actor.add(this._view.actor, { expand: true, x_fill: true, y_fill: true });
         this.actor.add(this._categoryScroll, { expand: false, y_fill: false, y_align: St.Align.START });
+        this.actor.add(this._view.actor, { expand: true, x_fill: true, y_fill: true });
 
         // Always select the "All" filter when switching to the app view
         this.actor.connect('notify::mapped', Lang.bind(this,
